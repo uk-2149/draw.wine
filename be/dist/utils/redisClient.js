ebb48b5c910de275359345c0c80bc821252a430b
pandarudra <rudrapanda8206@gmail.com>
2025-09-16 22:19:59 +0530

    be ++

--- be/dist/utils/redisClient.js
+++ be/dist/utils/redisClient.js
@@ -1,15 +1,12 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.sub = exports.pub = void 0;
-const redis_1 = require("redis");
-const redisClient = (0, redis_1.createClient)();
-redisClient.connect();
-// Publisher client
-const pub = redisClient.duplicate();
-exports.pub = pub;
-pub.connect();
-// Subscriber client
-const sub = redisClient.duplicate();
-exports.sub = sub;
-sub.connect();
-exports.default = redisClient;
+// import { createClient } from "redis";
+// const redisClient = createClient();
+// redisClient.connect();
+// // Publisher client
+// const pub = redisClient.duplicate();
+// pub.connect();
+// // Subscriber client
+// const sub = redisClient.duplicate();
+// sub.connect();
+// export { pub, sub };
+// export default redisClient;
