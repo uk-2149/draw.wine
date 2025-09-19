0f2ae83848081a720fd5f73babee3d880236d92e
pandarudra <rudrapanda8206@gmail.com>
2025-09-19 18:17:49 +0530

    updated

--- be/dist/routes/rooms.routes.js
+++ be/dist/routes/rooms.routes.js
@@ -1,8 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
-// import { CreateRoom, getRoomInfo } from "../controllers/rooms.controller";
 const roomRouter = (0, express_1.Router)();
-// roomRouter.post("/", CreateRoom);
-// roomRouter.get('/:roomId', getRoomInfo);
 exports.default = roomRouter;
--- be/dist/utils/redisClient.js
+++ be/dist/utils/redisClient.js
@@ -1,12 +1,1 @@
 "use strict";
-// import { createClient } from "redis";
-// const redisClient = createClient();
-// redisClient.connect();
-// // Publisher client
-// const pub = redisClient.duplicate();
-// pub.connect();
-// // Subscriber client
-// const sub = redisClient.duplicate();
-// sub.connect();
-// export { pub, sub };
-// export default redisClient;
